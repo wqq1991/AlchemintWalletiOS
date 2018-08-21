4e5ad67d138453d0bf925c316ee5b91bc96b89c4
wqq1991 <wqq1991@live.com>
2018-08-21 23:17:59 +0800

    lll
    s

---
+++ AlchemintProject/AlchemintProject/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  AlchemintProject
+//
+//  Created by WuQiaoqiao on 2018/8/17.
+//  Copyright © 2018年 com.alchemintTeam.wuqiaoqiao. All rights reserved.
+//
+
+import UIKit
+import Neoutils
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var wifTf: UITextField!
+    
+    @IBAction func loginBtnAction(_ sender: UIButton) {
+        
+        loginBtnAction()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func loginBtnAction() {
+        
+        
+        
+    }
+    
+
+}
+
